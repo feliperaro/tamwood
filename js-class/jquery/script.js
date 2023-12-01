d642daf18fb85fbb9951f0c6957249d717fe9bf8
FehRoque <feliperamosroque@gmail.com>
2023-11-30 19:08:12 -0500

    add script to script.js

--- js-class/jquery/script.js
+++ js-class/jquery/script.js
@@ -1,3 +1,25 @@
-
-
-console.log(jQuery().jQuery);
+console.log("jQuery", jQuery().jquery);
+let table = new DataTable("#example", {
+  scrollY: 300,
+  paging: false,
+  columnDefs: [
+    {
+      render: (data, type, row) => {
+        data = data.replace(data, data.split(" ")[0]);
+        return data;
+      },
+      targets: 0,
+    },
+    {
+      render: (data, type, row) => {
+        data = data.replace(
+          "Javascript Developer",
+          "<b>Javascript Developer</b>"
+        );
+        return data;
+      },
+      targets: 1,
+    },
+    { visible: false, targets: [3] },
+  ],
+});
