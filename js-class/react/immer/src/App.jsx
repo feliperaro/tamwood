0413c5e260814b7ef50ba45444077c2c61a2c317
FehRoque <feliperamosroque@gmail.com>
2023-12-20 17:12:07 -0500

    using produce for nextState

--- js-class/react/immer/src/App.jsx
+++ js-class/react/immer/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { produce } from "immer";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const baseState = [
+    {
+      title: "Learn TypeScript",
+      done: true,
+    },
+    {
+      title: "Try Immer",
+      done: false,
+    },
+  ];
+
+  const nextState = produce(baseState, (draft) => {
+    draft[1].done = true;
+    draft.push({ title: "Tweet about it" });
+    draft.push({ title: "Talk about it" });
+  });
+  console.log("nextState", nextState);
+
+  const students = [
+    { name: "Ying", color: "black" },
+    { name: "Yang", color: "white" },
+  ];
+
+  const nextStudents = produce(students, (draft) => {
+    draft.push({ name: "Poo", color: "blue" });
+    draft[1].color = "pink";
+  });
+
+  console.log("students", students);
+  console.log("nextStudents", nextStudents);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>IMMER</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
